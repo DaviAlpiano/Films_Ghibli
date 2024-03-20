137635ba7e6fc9447348d43cdf8c82f9bb33f642
Davi Alpiano <davialpiano@gmail.com>
2024-03-20 18:37:20 -0300

    Req 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
+import FilmsContext from './context/FilmsContext';
 
 function App() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    const FetchApi = async () => {
+      const dados = await fetch('https://api-trybe-frontend.vercel.app/api/ghibli-animations');
+      const resultado = await dados.json();
+      setFilmes(resultado);
+    };
+
+    FetchApi();
+  }, []);
+
+  console.log(filmes);
   return (
-    <Routes>
-      <Route path="/" Component={ Home } />
-      <Route path="/favorites" Component={ Favorites } />
-    </Routes>
+    <FilmsContext.Provider value={ { filmes } }>
+      <Routes>
+        <Route path="/" Component={ Home } />
+        <Route path="/favorites" Component={ Favorites } />
+      </Routes>
+    </FilmsContext.Provider>
   );
 }
 
--- src/context/FilmsContext.tsx
+++ src/context/FilmsContext.tsx
@@ -1,1 +1,29 @@
-// implemente a estrutura do context aqui.
+import { createContext } from 'react';
+
+type Film = {
+  id: string;
+  title: string;
+  original_title: string;
+  original_title_romanised: string;
+  image: string;
+  movie_banner: string;
+  description: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  running_time: string;
+  rt_score: string;
+  people: string[];
+  species: string[];
+  locations: string[];
+  vehicles: string[];
+  url: string;
+};
+
+type FilmsContextType = {
+  filmes: Film[],
+};
+
+const FilmsContext = createContext({} as FilmsContextType);
+
+export default FilmsContext;
