a14f2388d22ca83383c6e592459e76533afe5f05
Davi Alpiano <davialpiano@gmail.com>
2024-03-21 18:40:40 -0300

    Req 02

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import { useContext } from 'react';
+import FilmsContext from '../context/FilmsContext';
+import Header from '../components/Header';
+
 export default function Home() {
+  const { filmes } = useContext(FilmsContext);
   return (
     <div>
+      <Header />
       <h1>Films</h1>
+      {filmes.map((filme) => (
+        <div key={ filme.id }>
+          <h2>{filme.title}</h2>
+          <img src={ filme.movie_banner } alt={ filme.title } />
+          <button>Favorite</button>
+        </div>))}
     </div>
   );
 }
