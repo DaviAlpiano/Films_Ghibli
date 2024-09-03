a8bf49cd5de2b2017cacab6044ffa682ea015980
Davi Alpiano <davialpiano@gmail.com>
2024-09-03 17:44:22 -0300

    alteraçoes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 import style from './Footer.module.css';
 
 export default function Footer() {
-    return (
-        <footer className={ style.footer }>
-            <img src="src/img/castelo.png" alt="bolofofo" className={ style.castelo} />
-            <h1>STUDIO</h1>
-            <img src="src/img/bolofofo.png" alt="bolofofo" className={ style.bolofofo} />
-            <h1>GHIBLI</h1>
-            <img src="src/img/arte.jpg" alt="bolofofo" className={ style.arte} />
-        </footer>
-    )
+  return (
+    <footer className={ style.footer }>
+      <img src="src/img/castelo.png" alt="bolofofo" className={ style.castelo } />
+      <h1>STUDIO</h1>
+      <img src="src/img/bolofofo.png" alt="bolofofo" className={ style.bolofofo } />
+      <h1>GHIBLI</h1>
+      <img src="src/img/arte.jpg" alt="bolofofo" className={ style.arte } />
+    </footer>
+  );
 }
